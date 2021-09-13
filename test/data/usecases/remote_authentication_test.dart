0fa8d1c8abc2520d45455f5337dfa3b25059123b
Marcelo Garcia <marcelo.garcia@dragonvc.com.br>
2021-09-13 20:50:17 -0300

    doing tests to ensure that a valid response

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -26,6 +26,15 @@ void main(){
   });
 
   test('Should call HTTClient with correct values', () async{
+    when(httpClient?.request(
+        url: anyNamed('url') ,
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async => {
+      'accessToken':faker.guid.guid(),
+      'name': faker.person.name()
+    });
+
       await sut?.auth(params);
 
       verify(httpClient?.request(
@@ -83,4 +92,19 @@ void main(){
     expect(future, throwsA(DomainError.invalidCredentials));
   });
 
+  test('Should return an account if HttpClient returns 200', () async{
+    final accessToken = faker.guid.guid();
+    when(httpClient?.request(
+        url: anyNamed('url') ,
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async => {
+          'accessToken': accessToken,
+          'name': faker.person.name()
+        });
+
+    final account = await sut?.auth(params);
+
+    expect(account?.token, accessToken);
+  });
 }
